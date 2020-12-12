c04bad77084796229b1dbeb7e9e2ccaefa9fbcc8
semajson <jamessandy95@gmail.com>
2020-12-12 23:57:21 +0000

    for day8, improve performance by using a set

--- day8/part1.py
+++ day8/part1.py
@@ -49,6 +49,23 @@ def part1_solve(commands):
     return accumulator
 
 
+def part1_solve_set(commands):
+    commands_completed = set([])
+
+    index = 0
+    accumulator = 0
+    while True:
+        command = commands[index]
+
+        if command in commands_completed:
+            # Completed one loop, break
+            break
+
+        index, accumulator = command.do_command(index, accumulator)
+        commands_completed.add(command)
+    return accumulator
+
+
 def parse_input(input):
     commands = []
 
@@ -63,13 +80,13 @@ if __name__ == "__main__":
 
     # print(get_input("test_input"))
 
-    test_input = parse_input(get_input(PATH + "test_input"))
-    print(part1_solve(test_input))
+    # test_input = parse_input(get_input(PATH + "test_input"))
+    # print(part1_solve(test_input))
 
     real_input = parse_input(get_input(PATH + "real_input"))
-    print(part1_solve(real_input))
+    time_algo(part1_solve, real_input)
+    time_algo(part1_solve_set, real_input)
 
     # real_input = parse_input(get_input(PATH + "real_input"))
 
-    # real_input = parse_input(get_input("real_input"))
-    # print(part1_solve(real_input))
+    part1_solve
